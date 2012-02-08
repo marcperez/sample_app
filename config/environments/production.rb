a6212856729dea8bd4dcf1c7167c04264f0fc18f
Marc Pérez <marcperez2000@gmail.com>
2012-02-08 03:33:45 +0100

    blueprint precompile2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,10 +9,10 @@ SampleApp::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Precompile blueprint css
-  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
+  config.assets.precompile += %w( *.css *.js )
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
